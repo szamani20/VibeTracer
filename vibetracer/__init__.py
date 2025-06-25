a35128aeb6d361b70ad4c56e6fc54f93e1c5121b
szamani20 <szchem20@gmail.com>
2025-06-25 18:38:09 -0400

    Removed unnecessary get_engine from main init

--- vibetracer/__init__.py
+++ vibetracer/__init__.py
@@ -4,7 +4,6 @@
 VibeTracer public API—everything else is under the hood.
 """
 
-from .database.sqlite_db import get_engine
 from .trace.tracer import info_decorator
 
-__all__ = ["get_engine", "info_decorator"]
+__all__ = ["info_decorator"]
